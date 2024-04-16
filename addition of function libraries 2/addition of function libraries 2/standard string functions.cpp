78777a8184beb0278af19be57755542df4d15df0
sonixAnder <148888208+sonixAnder@users.noreply.github.com>
2024-04-16 15:26:34 +0300

    Add files via upload

---
+++ addition of function libraries 2/addition of function libraries 2/standard string functions.cpp
@@ -0,0 +1,67 @@
+﻿#include <iostream>
+#include <string>
+#include <cctype>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+    string text = "Some text with numbers 123 and letters abcd..";
+    string wordToReplace, newWord;
+
+    // 1. Найти и заменить слово в тексте
+    cout << text << endl;
+    cout << "Введите слово для замены: ";
+    cin >> wordToReplace;
+    cout << "Введите новое слово: ";
+    cin >> newWord;
+
+    size_t pos = text.find(wordToReplace);
+    if (pos != string::npos)
+    {
+        text.replace(pos, wordToReplace.length(), newWord);
+    }
+    cout << "Текст после замены: " << text << endl;
+
+    // 2. Каждое новое предложение с заглавной буквы
+    for (int i = 0; i < text.length(); i++)
+    {
+        if (i == 0 || text[i - 1] == '.' || text[i - 1] == '!' || text[i - 1] == '?')
+        {
+            text[i] = toupper(text[i]);
+        }
+    }
+    cout << "Текст, написанный с заглавными буквами каждого предложения: " << text << endl;
+
+    // 3. Подсчёт количества встречаемости каждой буквы в тексте
+    int letterCounts[26] = { 0 };
+    for (char c : text)
+    {
+        if (isalpha(c))
+        {
+            letterCounts[toupper(c) - 'A']++;
+        }
+    }
+
+    cout << "Вхождения каждой буквы:" << endl;
+    for (int i = 0; i < 26; i++)
+    {
+        if (letterCounts[i] > 0)
+        {
+            cout << (char)('A' + i) << ": " << letterCounts[i] << endl;
+        }
+    }
+
+    // 4. Подсчёт количества встречающихся цифр в тексте
+    int digitCount = 0;
+    for (char c : text)
+    {
+        if (isdigit(c))
+        {
+            digitCount++;
+        }
+    }
+    cout << "Число цифр в тексте: " << digitCount << endl;
+
+    return 0;
+}
---
+++ addition of function libraries/addition of function libraries/addition of function libraries.cpp
@@ -0,0 +1,93 @@
+﻿#include <iostream>
+#include <cstring>
+#include <cctype>
+#include <string.h>
+#include <string>
+using namespace std;
+
+//сравнение строк
+int mystrcmp(const char* str1, const char* str2)
+{
+    return strcmp(str1, str2);
+}
+
+//строка в число
+int StringToNumber(char* str)
+{
+    return stoi(str);
+}
+
+//число в строку
+char* NumberToString(int number)
+{
+    return strdup(to_string(number).c_str());
+}
+
+//верхний регистр
+char* Uppercase(char* str)
+{
+    for (int i = 0; str[i] != '\0'; i++)
+    {
+        str[i] = toupper(str[i]);
+    }
+    return str;
+}
+
+//нижний регистр
+char* Lowercase(char* str)
+{
+    for (int i = 0; str[i] != '\0'; i++)
+    {
+        str[i] = tolower(str[i]);
+    }
+    return str;
+}
+
+//ревёрс строки
+char* mystrrev(char* str)
+{
+    int len = strlen(str);
+    char* result = new char[len + 1];
+
+    for (int i = 0; i < len; i++)
+    {
+        result[i] = str[len - i - 1];
+    }
+
+    result[len] = '\0';
+    return result;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+    const char* str1 = "hello";
+    const char* str2 = "world";
+
+    cout << "сраввнение строк: " << mystrcmp(str1, str2) << endl;
+    cout << "----------------------------------------" << endl;
+
+    char numStr[] = "12345";
+    int num = StringToNumber(numStr);
+    cout << "строка в число: " << num << endl;
+    cout << "----------------------------------------" << endl;
+
+    char* strNum = NumberToString(54321);
+    cout << "число в строку: " << strNum << endl;
+    cout << "----------------------------------------" << endl;
+
+    char text[] = "abc123";
+    Uppercase(text);
+    cout << "верхний регистр: " << text << endl;
+    cout << "----------------------------------------" << endl;
+
+    Lowercase(text);
+    cout << "нижний регистр: " << text << endl;
+    cout << "----------------------------------------" << endl;
+
+    char rev[] = "reverse";
+    char* reversedStr = mystrrev(rev);
+    cout << "ревёрс строки: " << reversedStr << endl;
+
+    return 0;
+}
